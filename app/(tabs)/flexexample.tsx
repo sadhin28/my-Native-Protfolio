af2b26e014248f902060713656736fbca647f721
sadhin28 <taosifsadhin113@gmail.com>
2025-10-31 12:42:55 +0600

    practice in felexbox

--- app/(tabs)/flexexample.tsx
+++ app/(tabs)/flexexample.tsx
@@ -10,9 +10,9 @@ export default function flexexample() {
           flexDirection: 'column',
         },
       ]}>
-      <View style={{flex: 1, backgroundColor: 'red'}} />
+      <View style={{flex: 3, backgroundColor: 'red'}} />
       <View style={{flex: 2, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 3, backgroundColor: 'green'}} />
+      <View style={{flex: 1, backgroundColor: 'green'}} />
     </View>
   )
 }
