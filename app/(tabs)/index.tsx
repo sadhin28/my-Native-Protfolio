993e82d705bbbb73dbf5c60cfc64cbe50c6500d0
sadhin28 <taosifsadhin113@gmail.com>
2025-10-31 12:31:26 +0600

    add index in the home button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View>
-          <Text>Hellow World</Text>
+        <View style={styles.container}>
+          <Text style={styles.name}>Taosif Sadhin</Text>
+          <View style={{alignItems: "center"}}>
+             <Image src="" alt="Img"/>
+              <Text style={styles.name}>Sample Image</Text>
+          </View>
         </View>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection:"row",
+    justifyContent:"space-around",
+    marginTop:0,
+    alignItems: "center",
+    backgroundColor: "#ff4f00",
+    padding: 16,
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: "#ffffff",
+    
+  },
+})
