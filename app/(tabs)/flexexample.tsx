b08db738a75d9e8411911111fd08ab3de9bbac7c
sadhin28 <taosifsadhin113@gmail.com>
2025-11-01 18:29:22 +0600

    add new view

--- app/(tabs)/flexexample.tsx
+++ app/(tabs)/flexexample.tsx
@@ -10,9 +10,10 @@ export default function flexexample() {
           flexDirection: 'column',
         },
       ]}>
-      <View style={{flex: 3, backgroundColor: 'red'}} />
-      <View style={{flex: 2, backgroundColor: 'darkorange'}} />
-      <View style={{flex: 1, backgroundColor: 'green'}} />
+      <View style={{flex: 4, backgroundColor: 'red'}} />
+      <View style={{flex: 3, backgroundColor: 'darkorange'}} />
+      <View style={{flex: 2, backgroundColor: 'green'}} />
+      <View style={{flex: 1, backgroundColor: 'blue'}} />
     </View>
   )
 }
